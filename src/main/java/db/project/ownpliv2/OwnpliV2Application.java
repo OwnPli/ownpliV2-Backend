3c8c6b3a49aa8996b1dc32c4dc519781c6b500d8
ahma0 <dsd932@naver.com>
2023-08-24 14:04:51 +0900

    프로젝트 생성

---
+++ src/main/java/db/project/ownpliv2/OwnpliV2Application.java
@@ -0,0 +1,13 @@
+package db.project.ownpliv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnpliV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnpliV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/db/project/ownpliv2/OwnpliV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package db.project.ownpliv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnpliV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
