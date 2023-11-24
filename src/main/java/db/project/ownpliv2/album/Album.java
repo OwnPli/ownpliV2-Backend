e9691faf7e6c8f8d2b29fb906693862c5c69c5f6
ahma0 <dsd932@naver.com>
2023-11-25 00:35:06 +0900

    feat: album entity 생성

---
+++ src/main/java/db/project/ownpliv2/album/Album.java
@@ -0,0 +1,40 @@
+package db.project.ownpliv2.album;
+
+import db.project.ownpliv2.album.dto.AlbumMessage;
+import db.project.ownpliv2.common.domain.BaseEntity;
+import db.project.ownpliv2.common.domain.value.Domain;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Table(name = "album")
+@Getter
+public class Album extends BaseEntity {
+
+    @Column(nullable = false)
+    private String albumTitle;
+
+    @Column(nullable = false)
+    private String albumImage;
+
+    protected Album() {
+        super(Domain.ALBUM);
+    }
+
+    @Builder
+    public Album(String albumTitle, String albumImage) {
+        super(Domain.ALBUM);
+        this.albumTitle = albumTitle;
+        this.albumImage = albumImage;
+    }
+
+    public static Album of(AlbumMessage albumMessage) {
+        return Album.builder()
+                .albumTitle(albumMessage.albumTitle())
+                .albumImage(albumMessage.image())
+                .build();
+    }
+}
