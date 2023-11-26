9737324067396228d033baa60cf5e13e7624bd65
ahma0 <dsd932@naver.com>
2023-11-26 21:26:00 +0900

    feat: Artist Repository 생성

--- src/main/java/db/project/ownpliv2/repository/AlbumRepository.java
+++ src/main/java/db/project/ownpliv2/repository/AlbumRepository.java
@@ -1,6 +1,7 @@
 package db.project.ownpliv2.repository;
 
+import db.project.ownpliv2.album.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlbumRepository extends JpaRepository {
+public interface AlbumRepository extends JpaRepository<Album, String> {
 }
---
+++ src/main/java/db/project/ownpliv2/repository/ArtistRepository.java
@@ -0,0 +1,7 @@
+package db.project.ownpliv2.repository;
+
+import db.project.ownpliv2.artist.domain.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistRepository extends JpaRepository<Artist, String> {
+}
