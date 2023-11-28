c4c3aaf53620f8f7967c40c43c08c657096b5e48
ahma0 <dsd932@naver.com>
2023-11-28 15:21:54 +0900

    feat: RedisConfig 생성

---
+++ src/main/java/db/project/ownpliv2/config/redis/RedisConfig.java
@@ -0,0 +1,21 @@
+package db.project.ownpliv2.config.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@ConfigurationProperties(prefix = "redis")
+public class RedisConfig {
+
+    private String host;
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+}
