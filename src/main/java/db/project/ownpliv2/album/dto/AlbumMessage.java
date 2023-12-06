51b6f2b4bca8cb7d8334cb6864e80799dae9b492
ahma0 <dsd932@naver.com>
2023-12-06 23:40:09 +0900

    feat: implements Serializable

--- src/main/java/db/project/ownpliv2/album/dto/AlbumMessage.java
+++ src/main/java/db/project/ownpliv2/album/dto/AlbumMessage.java
@@ -2,8 +2,10 @@ package db.project.ownpliv2.album.dto;
 
 import db.project.ownpliv2.artist.dto.ArtistMessage;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 
-public record AlbumMessage(String albumTitle, List<ArtistMessage> artist, String image, LocalDate releaseDate, String spotifyKey) {
+public record AlbumMessage(String albumTitle, List<ArtistMessage> artist, String image, LocalDate releaseDate, String spotifyKey)
+        implements Serializable {
 }
--- src/main/java/db/project/ownpliv2/artist/dto/ArtistMessage.java
+++ src/main/java/db/project/ownpliv2/artist/dto/ArtistMessage.java
@@ -1,5 +1,7 @@
 package db.project.ownpliv2.artist.dto;
 
-public record ArtistMessage(String artistKey, String artistName) {
+import java.io.Serializable;
+
+public record ArtistMessage(String artistKey, String artistName) implements Serializable {
 
 }
